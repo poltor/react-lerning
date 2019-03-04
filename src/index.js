40cd5b1845a0250fef10fc2d03f94ac2701671f7
poltor <v.poltoratsky@gmail.com>
2019-03-04 17:40:16 +0300

    Step 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello world!</div>, document.getElementById('root'));
+ReactDOM.render('Hello world!', document.getElementById('root'));
+
+
+// const element = React.createElement('div', '',  'Hello world!');
+// ReactDOM.render(element, document.getElementById('root'));
+
+
+// ReactDOM.render(<div>Hello world!</div>, document.getElementById('root'));
+
+
+

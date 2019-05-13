34a9cb378c1f85352d48823a6cbeb54391effb4d
poltor <poltoratskiy@tecforce.ru>
2019-05-13 17:01:42 +0300

    Step 9

---
+++ src/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux'
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.text]);
+    default:
+      return state
+  }
+}
+
+export default function() {
+  const store = createStore(todos, ['Use Redux']);
+  return store;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
+import configureStore from './configureStore';
+import { App } from './App';
+
+
+const store = configureStore();
+
+console.log(store);
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
